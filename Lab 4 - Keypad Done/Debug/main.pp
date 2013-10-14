95d7aad8bfce4fd20e8899fac56a975896b7b9e6
luzonj <jesus.luzon@upr.edu>
2013-10-13 21:01:25 -0400

    Initial commit.

---
+++ Lab 4 - Keypad Done/Debug/main.pp
@@ -0,0 +1,39 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdint.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdbool.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/yvals.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdarg.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/linkage.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/_lock.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/string.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/inc/tm4c1230h6pm.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/sysctl.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/math.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/ymath.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/cpu.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/timer.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/gpio.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/interrupt.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/inc/hw_memmap.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/inc/hw_types.h
+
+../main.c: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdint.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdbool.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/yvals.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/_lock.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/string.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/inc/tm4c1230h6pm.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/sysctl.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/math.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/ymath.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/cpu.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/timer.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/gpio.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/interrupt.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/inc/hw_memmap.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/inc/hw_types.h: 
---
+++ Lab 4 - Keypad Done/main.c
@@ -0,0 +1,239 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include "inc/tm4c1230h6pm.h"
+#include "driverlib/sysctl.h"
+#include <math.h>
+#include "driverlib/cpu.h"
+//#include "inc/hw_ints.h"
+#include "driverlib/timer.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+
+char values[16];
+char buttons[4][3];
+char portBStates[4] = { 1,2,4,8 };
+char portBCurrState = 0;
+char lastKeysPressed[2] = { 0 , 0 };
+char lastPressedKey = 0;
+char currSevenSeg = 0;
+char keypadScanCurr = 0;
+void delay(int delay) {
+	SysCtlDelay(delay);
+}
+
+void FillValuesArray() {
+	values[0] = 0xD7;
+	values[1] = 0x84;
+	values[2] = 0xE3;
+	values[3] = 0xE6;
+	values[4] = 0xB4;
+	values[5] = 0x76;
+	values[6] = 0x77;
+	values[7] = 0xC4;
+	values[8] = 0xF7;
+	values[9] = 0xF4;
+	values[0xA] = 0xF5;
+	values[0xB] = 0xFF;
+	values[0xC] = 0x53;
+	values[0xD] = 0xDF;
+	values[0xE] = 0x73;
+	values[0xF] = 0x71;
+}
+
+void FillKeypadButtonsArray() {
+
+	buttons[0][0] = 1;
+	buttons[0][1] = 2;
+	buttons[0][2] = 3;
+	buttons[1][0] = 4;
+	buttons[1][1] = 5;
+	buttons[1][2] = 6;
+	buttons[2][0] = 7;
+	buttons[2][1] = 8;
+	buttons[2][2] = 9;
+	buttons[3][0] = 0xA;;
+	buttons[3][1] = 0;
+	buttons[3][2] = 0xB;
+}
+void init() {
+
+	SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOA;
+	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOB;
+	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOC;
+	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOD;
+	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOE;
+	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOF;
+
+	GPIO_PORTA_DIR_R = 0x00;
+	GPIO_PORTB_DIR_R = 0x00;
+	GPIO_PORTC_DIR_R = 0x00;
+	GPIO_PORTD_DIR_R = 0x00;
+	GPIO_PORTE_DIR_R = 0x00;
+	GPIO_PORTF_DIR_R = 0x00;
+
+	GPIO_PORTA_DEN_R = 0x00;
+	GPIO_PORTB_DEN_R = 0x00;
+	GPIO_PORTC_DEN_R = 0x00;
+	GPIO_PORTD_DEN_R = 0x00;
+	GPIO_PORTE_DEN_R = 0x00;
+	GPIO_PORTF_DEN_R = 0x00;
+
+	GPIO_PORTA_DATA_R = 0x00;
+	GPIO_PORTB_DATA_R = 0x00;
+	GPIO_PORTC_DATA_R = 0x00;
+	GPIO_PORTD_DATA_R = 0x00;
+	GPIO_PORTE_DATA_R = 0x00;
+	GPIO_PORTF_DATA_R = 0x00;
+
+	SYSCTL_RCGC2_R = 0x00;
+}
+
+void SevenSegInit() {
+	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOC;
+	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOD;
+	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOA;
+
+	GPIO_PORTC_DIR_R = 0xF0;
+	GPIO_PORTC_DEN_R = 0xF0;
+	GPIO_PORTC_DATA_R = 0x00;
+
+	GPIO_PORTD_DIR_R = 0x0F;
+	GPIO_PORTD_DEN_R = 0x0F;
+	GPIO_PORTD_DATA_R = 0x00;
+
+	GPIO_PORTA_DIR_R = 0x0C;
+	GPIO_PORTA_DEN_R = 0x0C;
+	GPIO_PORTA_DATA_R = 0x0C;
+}
+void KeypadInterruptEnable() {
+	IntEnable(INT_GPIOE);
+	HWREG(0x40024408)= 0x00;
+	HWREG(0x4002440C) = 0x0E;
+	HWREG(0x40024410) = 0x0E;
+
+}
+void KeypadInit() {
+	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOB;
+	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOE;
+
+	GPIO_PORTB_DIR_R = 0x0F;
+	GPIO_PORTB_DEN_R = 0x0F;
+	GPIO_PORTB_DATA_R = 0x01;
+
+	GPIO_PORTE_DIR_R = 0x00;
+	GPIO_PORTE_DEN_R = 0x0E;
+	GPIO_PORTE_DATA_R = 0x00;
+
+	//keypadScanCurr = 0x01;
+
+	FillKeypadButtonsArray();
+	KeypadInterruptEnable();
+}
+void PrintNumber(char num) {
+	GPIO_PORTC_DATA_R = ~(values[num] & 0xF0);
+	GPIO_PORTD_DATA_R = ~(values[num] & 0x0F);
+
+}
+
+void InitTimerModule()
+{
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+	SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+	TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
+	TimerLoadSet(TIMER0_BASE, TIMER_A, SysCtlClockGet()/20);
+	IntEnable(INT_TIMER0A);
+	TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+	TimerEnable(TIMER0_BASE, TIMER_A);
+
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
+	TimerConfigure(TIMER1_BASE, TIMER_CFG_PERIODIC);
+	TimerLoadSet(TIMER1_BASE, TIMER_A, SysCtlClockGet()/120);
+	IntEnable(INT_TIMER1A);
+	TimerIntEnable(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+	TimerEnable(TIMER1_BASE, TIMER_A);
+}
+
+void TimerInterruptSevenSegmentDisplay() {
+	TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+	//PrintNumber(sevenSegVals[currSevenSeg]);
+	PrintNumber(lastKeysPressed[currSevenSeg]);
+	GPIO_PORTA_DATA_R ^= 0x0C;
+	currSevenSeg ^= 1;
+}
+
+int FindKeypadButtonPressed(char num) {
+	switch (GPIO_PORTE_DATA_R & 0x0E) {
+	case 0x02:
+		return buttons[num][0];
+	case 0x04:
+		return buttons[num][1];
+	case 0x08:
+		return buttons[num][2];
+	default:
+		return -1;
+	}
+}
+void TimerInterruptKeypadScan() {
+	TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+
+	portBCurrState++;
+	portBCurrState %= 4;
+	GPIO_PORTB_DATA_R = portBStates[portBCurrState];
+
+}
+void InterruptGPIOEPressed() {
+	//portBCurrState += 3;
+	//portBCurrState %= 4;
+	//GPIO_PORTB_DATA_R = portBStates[portBCurrState];
+	delay(1000);
+	HWREG(0x4002441C) = 0xFF; //clear interrupt flag signal
+	int keyPressed = -1;
+	switch (GPIO_PORTB_DATA_R & 0x0F) {
+	case 0x01:
+		keyPressed = FindKeypadButtonPressed(0);
+		break;
+	case 0x02:
+		keyPressed = FindKeypadButtonPressed(1);
+		break;
+	case 0x04:
+		keyPressed = FindKeypadButtonPressed(2);
+		break;
+	case 0x08:
+		keyPressed = FindKeypadButtonPressed(3);
+		break;
+	default:
+		keyPressed = -1;
+
+	}
+
+	if((keyPressed + 1)) {
+		lastKeysPressed[0] = lastKeysPressed[1];
+		lastKeysPressed[1] = keyPressed;
+	}
+
+}
+
+int main(void) {
+
+	init();
+	SevenSegInit();
+	KeypadInit();
+	FillValuesArray();
+	IntMasterEnable();
+
+	GPIO_PORTA_DATA_R = 0x04;
+	lastKeysPressed[0] = 0;
+	lastKeysPressed[1] = 0;
+	currSevenSeg = 0;
+	InitTimerModule();
+	HWREG(NVIC_SYS_CTRL_R) |= NVIC_SYS_CTRL_SLEEPEXIT;
+	while(1);
+
+
+
+}
+
+
